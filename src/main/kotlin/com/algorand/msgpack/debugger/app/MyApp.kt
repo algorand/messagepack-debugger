ce8727cac5c41afe292894d414a1703a13436fcb
Will Winder <wwinder.unh@gmail.com>
2020-02-10 10:29:36 -0500

    Create jar with dependencies.

--- src/main/kotlin/com/algorand/msgpack/debugger/app/MyApp.kt
+++ src/main/kotlin/com/algorand/msgpack/debugger/app/MyApp.kt
@@ -1,6 +1,12 @@
 package com.algorand.msgpack.debugger.app
 
 import com.algorand.msgpack.debugger.view.MainView
-import tornadofx.App
+import javafx.application.Application
+import tornadofx.*
 
 class MyApp: App(MainView::class, Styles::class)
+
+fun main(args: Array<String>) {
+    Application.launch(MyApp::class.java, *args)
+}
+
