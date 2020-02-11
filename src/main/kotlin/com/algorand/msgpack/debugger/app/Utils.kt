49d3061f8d0b0739eae51a88d374b4ae252dcb59
Will Winder <wwinder.unh@gmail.com>
2020-02-11 10:41:58 -0500

    Allow loading a binary file as well.

--- src/main/kotlin/com/algorand/msgpack/debugger/app/Utils.kt
+++ src/main/kotlin/com/algorand/msgpack/debugger/app/Utils.kt
@@ -3,15 +3,33 @@ package com.algorand.msgpack.debugger.app
 import com.algorand.algosdk.util.Encoder
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.commons.codec.binary.Base64
 import org.msgpack.jackson.dataformat.MessagePackFactory
 import java.io.ByteArrayInputStream
+import java.nio.file.Files
+import java.nio.file.Path
 
+// The input could be a base64 encoded string or a file path with bytes that need encoding or base64 data.
+private fun getDataFromInput(str: String): String {
+    return if (Files.exists(Path.of(str))) {
+        val fileData = Files.readAllBytes(Path.of(str))
+        val stringValue = fileData.toString(Charsets.UTF_8)
+        if (Base64.isBase64(stringValue)) {
+            stringValue
+        } else {
+            Encoder.encodeToBase64(fileData)
+        }
+    } else {
+        str
+    }
+}
 fun unpack(str: String): Map<*, *> {
+    val processedStr = getDataFromInput(str)
     val ret = mutableMapOf<Any, Any>()
     var objectNum = 1
 
     val factory = MessagePackFactory()
-    val inputStream = ByteArrayInputStream(Encoder.decodeFromBase64(str))
+    val inputStream = ByteArrayInputStream(Encoder.decodeFromBase64(processedStr))
     val parser = factory.createParser(inputStream)
     val mapper = ObjectMapper(factory)
     mapper.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
